17f92a30f9e5407271bc5a0b4fcbeecd1df20d9f
Manikanta <84836313+manikantabadugu@users.noreply.github.com>
2022-05-18 23:46:38 +0200

    Update main.py

--- handrecognition/main.py
+++ handrecognition/main.py
@@ -38,8 +38,8 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
         if results.multi_hand_landmarks:
             for num, hand in enumerate(results.multi_hand_landmarks):
                 mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
-                                          mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
-                                          mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
+                                          mp_drawing.DrawingSpec(color=(0,0,0), thickness=2, circle_radius=4),
+                                          mp_drawing.DrawingSpec(color=(0, 255, 255), thickness=2, circle_radius=2),
                                           )
 
         cv2.imshow('Hand Tracking', image)
