a59b6e2e7f87744a41e2651c42182797507fd611
Manikanta <84836313+manikantabadugu@users.noreply.github.com>
2022-05-19 12:27:22 +0200

    Update main.py

--- handrecognition/main.py
+++ handrecognition/main.py
@@ -1,11 +1,14 @@
-import mediapipe as mp
 import cv2
-import numpy as np
-import uuid
 import os
+import mediapipe as mp
+import uuid
+
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
+#run the below line, when the code is running for first time, later it can be commented out.
+
+#os.mkdir('Output Images')
 
 cap = cv2.VideoCapture(0)
 
@@ -13,7 +16,6 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
     while cap.isOpened():
         ret, frame = cap.read()
 
-        # BGR 2 RGB
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         # Flip on horizontal
@@ -22,76 +24,25 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
         # Set flag
         image.flags.writeable = False
 
-        # Detections
-        results = hands.process(image)
+        # passing the feed to mediapipe to find the hand
+        result = hands.process(image)
 
         # Set flag to true
         image.flags.writeable = True
 
-        # RGB 2 BGR
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        #print(results)
 
-        # Detections
-        print(results)
-
-        # Rendering results
-        if results.multi_hand_landmarks:
-            for num, hand in enumerate(results.multi_hand_landmarks):
+        # embedding the detected points of the hands on to the feed
+        if result.multi_hand_landmarks:
+            for num, hand in enumerate(result.multi_hand_landmarks):
                 mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
                                           mp_drawing.DrawingSpec(color=(0,0,0), thickness=2, circle_radius=4),
                                           mp_drawing.DrawingSpec(color=(0, 225, 225), thickness=2, circle_radius=2),
                                           )
-
-        cv2.imshow('Hand Tracking', image)
-
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-
-cap.release()
-cv2.destroyAllWindows()
-
-
-
-os.mkdir('Output Images')
-
-cap = cv2.VideoCapture(0)
-
-with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
-    while cap.isOpened():
-        ret, frame = cap.read()
-
-        # BGR 2 RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        # Flip on horizontal
-        image = cv2.flip(image, 1)
-
-        # Set flag
-        image.flags.writeable = False
-
-        # Detections
-        results = hands.process(image)
-
-        # Set flag to true
-        image.flags.writeable = True
-
-        # RGB 2 BGR
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        # Detections
-        print(results)
-
-        # Rendering results
-        if results.multi_hand_landmarks:
-            for num, hand in enumerate(results.multi_hand_landmarks):
-                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
-                                          mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
-                                          mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
-                                          )
-
-        # Save our image
-        cv2.imwrite(os.path.join('Output Images', '{}.jpg'.format(uuid.uuid1())), image)
-        cv2.imshow('Hand Tracking', image)
+                # Saving the results, this line can be commented if you don't want to save results
+                cv2.imwrite(os.path.join('Output Images', '{}.jpg'.format(uuid.uuid1())), image)
+        cv2.imshow('Hand Recognition', image)
 
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
